e13ded60bd70a263dd07fa1391e610dc25481f41
Al-del <david.david78dd@gmail.com>
2025-07-02 11:56:46 +0300

    firs_commit

--- app/src/main/java/com/example/shortmeal/Navigation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shortmeal
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-
-@Composable
-fun Navigation() {
-
-}
